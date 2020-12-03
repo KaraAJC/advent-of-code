f863ecabf941d6df4b1b04ee6f5c66b88431290c
KaraAJC <KaraAJC@gmail.com>
2020-12-02 20:24:39 -0600

    Solution refactored to handle part two

--- 2020/2/solution.rb
+++ 2020/2/solution.rb
@@ -23,22 +23,36 @@ class PasswordChecker
     parsed
   end
 
-  def validate_password(log)
-    puts "there are #{log[:password].count(log[:letter])} letter #{log[:letter]}'s"
-    puts "rules are minimum #{log[:min]} and max #{log[:max]}"
+  def validate_password_frequency(log)
+    # puts "rules are minimum #{log[:min]} and max #{log[:max]}"
+    # puts "there are #{log[:password].count(log[:letter])} letter #{log[:letter]}'s"
     log[:password].count(log[:letter]).to_i.between?(log[:min],log[:max])
   end
 
-  def check_passwords
+  def validate_password_accuracy(log)
+    return false unless log[:password].include?(log[:letter])
+    return false if log[:password][log[:min] - 1] == log[:letter] && log[:password][log[:max] - 1] == log[:letter]
+
+    # puts "rules are letter #{log[:letter]} must be at position #{log[:min]} and then at position #{log[:max]}"
+    # puts "there's a #{log[:password][log[:min] - 1]} at position #{log[:min]} and a #{log[:password][log[:max] - 1]} at position #{log[:max]}"
+    log[:password][log[:min] - 1] == log[:letter] || log[:password][log[:max] - 1] == log[:letter]
+  end
+
+  def check_passwords(way="frequency")
     @parsed_passwords.map do |log|
-      validate_password(log)
-   end.count(true)
+        if way == "accuracy"
+          validate_password_accuracy(log)
+        else
+          validate_password_frequency(log)
+        end
+    end.count(true)
   end
 
 end
 
 check = PasswordChecker.new(INPUT)
 
-puts check.parse_passwords
-puts "======="
-puts "#{check.check_passwords} are valid"
+# puts check.parse_passwords
+# puts "======="
+puts "#{check.check_passwords} are valid for part one"
+puts "#{check.check_passwords("accuracy")} are valid for part two"
